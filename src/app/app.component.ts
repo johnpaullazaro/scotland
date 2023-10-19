eecfee2b27c162da2bec776ba0a2835ca11c87a1
Erman Louie Faminiano <ermanfaminiano020@gmail.com>
2023-10-19 19:22:29 +0800

    added tailwind and flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Scotland-Forked';
+
+  ngOnInit(): void{
+    initFlowbite();
+  }
 }
+
